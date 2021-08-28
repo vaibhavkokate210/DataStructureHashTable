29a645f2f634bc3e0b0ba903812c64bb086f7ba9
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-28 23:43:44 +0530

    [Vaibhav] added ability to find frequerncy of words in paragraph

--- src/com/datastructure/hashmap/HashMapRunner.java
+++ src/com/datastructure/hashmap/HashMapRunner.java
@@ -4,7 +4,7 @@ public class HashMapRunner
 {
 	public static void findFrequency()
 	{
-		String str="To be or not to be";
+		String str="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 		String myStr[]=str.split(" ");
 		MyHashMap<String,Integer> myMap=new MyHashMap();
 		for(String wd:myStr)
